edfd336dd50511c3ebe5d7e4675cd32b89d52dcd
devsujalpatel <sujal7455@gmail.com>
2025-10-14 04:59:54 +0530

    testing trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
-import { Button } from "@/components/ui/button";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { Client } from "./client";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
-export default function Home() {
-  return <div className="flex h-screen items-center justify-center">
-    <Button className="shadow cursor-pointer">Click me</Button>
-  </div>;
+export default async function Home() {
+  const queryClient = getQueryClient();
+
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
+
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Client />
+      </HydrationBoundary>
+    </div>
+  );
 }
