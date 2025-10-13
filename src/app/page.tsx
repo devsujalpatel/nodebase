bdd37379b0afc0ba4f376988a34a7a37fededddb
devsujalpatel <sujal7455@gmail.com>
2025-10-14 05:12:30 +0530

    Suspene show

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { getQueryClient, trpc } from "@/trpc/server";
 import { Client } from "./client";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
 
 export default async function Home() {
   const queryClient = getQueryClient();
@@ -10,7 +11,9 @@ export default async function Home() {
   return (
     <div className="flex h-screen items-center justify-center">
       <HydrationBoundary state={dehydrate(queryClient)}>
-        <Client />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Client />
+        </Suspense>
       </HydrationBoundary>
     </div>
   );
