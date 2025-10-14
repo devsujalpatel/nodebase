a5d65619f2df7472a58d6e287f58709666aa20df
devsujalpatel <sujal7455@gmail.com>
2025-10-14 23:21:58 +0530

    Login form and register form

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import { LoginForm } from "@/app/features/auth/components/login-form";
+import React from "react";
+
+const Page = () => {
+  return <div>
+    <LoginForm />
+  </div>;
+};
+
+export default Page;
