1206834d4b08cac1720c5d37ce33d109b467f89c
devsujalpatel <sujal7455@gmail.com>
2025-10-15 02:46:36 +0530

    protecting the data with trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
+import { Button } from "@/components/ui/button";
 import { requireAuth } from "@/lib/auth-utils";
+import { caller } from "@/trpc/server";
 
 export default async function Home() {
   await requireAuth();
+
+  const data = await caller.getUsers();
+
   return (
     <div className="flex h-screen items-center justify-center">
       Protected server component
+      {JSON.stringify(data[0].name)}
     </div>
   );
 }
