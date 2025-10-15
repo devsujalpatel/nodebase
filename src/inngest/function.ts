8ac08794e07d45a26046af8c87436ea179f29fec
devsujalpatel <sujal7455@gmail.com>
2025-10-15 17:01:26 +0530

    Options for openai, anthropic

--- src/inngest/function.ts
+++ src/inngest/function.ts
@@ -12,12 +12,38 @@ export const execute = inngest.createFunction(
   { id: "execute-ai" },
   { event: "execute/ai" },
   async ({ event, step }) => {
-    const { steps: geminiSteps } = await step.ai.wrap("gemini-generate-text", generateText, {
-      model: google("gemini-2.5-flash"),
-      system: "you are a helpfull assistant",
-      prompt: "what is 2 + 2",
-    });
+    const { steps: geminiSteps } = await step.ai.wrap(
+      "gemini-generate-text",
+      generateText,
+      {
+        model: google("gemini-2.5-flash"),
+        system: "you are a helpfull assistant",
+        prompt: "what is 2 + 2",
+      }
+    );
+    const { steps: openaiSteps } = await step.ai.wrap(
+      "openai-generate-text",
+      generateText,
+      {
+        model: openai("gpt-4"),
+        system: "you are a helpfull assistant",
+        prompt: "what is 2 + 2",
+      }
+    );
+    const { steps: anthropicSteps } = await step.ai.wrap(
+      "anthropic-generate-text",
+      generateText,
+      {
+        model: anthropic("claude-sonnet-4-5"),
+        system: "you are a helpfull assistant",
+        prompt: "what is 2 + 2",
+      }
+    );
 
-    return steps;
+    return {
+      geminiSteps,
+      openaiSteps,
+      anthropicSteps,
+    };
   }
 );
