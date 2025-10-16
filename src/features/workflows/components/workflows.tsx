9a869cb8a33412d26937cc72d78a0b1158f3c5ad
devsujalpatel <sujal7455@gmail.com>
2025-10-17 04:30:03 +0530

    workflows router created

---
+++ src/features/workflows/components/workflows.tsx
@@ -0,0 +1,7 @@
+"use client"
+import { useSuspenseWorkflows } from "../hooks/use-workflows";
+
+export const WorkflowsList = () => {
+  const workflows = useSuspenseWorkflows();
+  return <p>{JSON.stringify(workflows.data, null, 2)}</p>;
+};
