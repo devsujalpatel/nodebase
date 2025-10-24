02fb9fc0261fa25fbb804f0cdf150b026d0ee817
devsujalpatel <sujal7455@gmail.com>
2025-10-25 01:04:17 +0530

    Editor state half done with a error

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -22,6 +22,8 @@ import {
 import "@xyflow/react/dist/style.css";
 import { nodeComponents } from "@/config/node-components";
 import { AddNodeButton } from "./add-node-button";
+import { useSetAtom } from "jotai";
+import { editorAtom } from "../store/atoms";
 
 export const EditorLoading = () => {
   return <LoadingView message="Loading editor..." />;
@@ -33,6 +35,9 @@ export const EditorError = () => {
 
 export const Editor = ({ workflowId }: { workflowId: string }) => {
   const { data: workflow } = useSuspenseWorkflow(workflowId);
+
+  const setEditor = useSetAtom(editorAtom);
+
   const [nodes, setNodes] = useState<Node[]>(workflow.nodes);
   const [edges, setEdges] = useState<Edge[]>(workflow.edges);
 
@@ -62,6 +67,7 @@ export const Editor = ({ workflowId }: { workflowId: string }) => {
         onConnect={onConnect}
         fitView
         nodeTypes={nodeComponents}
+        onInit={setEditor}
         proOptions={{
           hideAttribution: true,
         }}
--- src/features/workflows/hooks/use-workflows.ts
+++ src/features/workflows/hooks/use-workflows.ts
@@ -55,6 +55,9 @@ export const useUpdateWorkflowName = () => {
   );
 };
 
+
+
+
 export const useRemoveWorkflow = () => {
   const trpc = useTRPC();
   const queryClient = useQueryClient();
@@ -83,6 +86,31 @@ export const useSuspenseWorkflow = (id: string) => {
   return useSuspenseQuery(
     trpc.workflows.getOne.queryOptions({
       id,
+    })
+  );
+};
+
+
+/**
+ * Hook to update a workflow
+ */
+
+export const useUpdateWorkflow = () => {
+  const queryClient = useQueryClient();
+  const trpc = useTRPC();
+
+  return useMutation(
+    trpc.workflows.update.mutationOptions({
+      onSuccess: (data) => {
+        toast.success(`Workflow ${data.name} saved`);
+        queryClient.invalidateQueries(trpc.workflows.getMany.queryOptions({}));
+        queryClient.invalidateQueries(
+          trpc.workflows.getOne.queryOptions({ id: data.id })
+        );
+      },
+      onError: (err) => {
+        toast.error(`Failed to update workflow: ${err.message}`);
+      },
     })
   );
 };
